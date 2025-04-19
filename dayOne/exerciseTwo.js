59552f5f261b112e51891eaea07c0c282f95eae7
yahya-mohamoud <yahyamohamoud625@gmail.com.com>
2025-04-19 13:02:41 +0300

    second exercise finished

---
+++ dayOne/exerciseTwo.js
@@ -0,0 +1,12 @@
+const sideOne = 5;
+const sideTwo = 6;
+const sideThree = 7;
+
+const s = (sideOne + sideTwo + sideThree) / 2;
+
+const area = Math.sqrt(s * (s - sideOne) * (s - sideTwo) * (s - sideThree))
+
+
+
+
+console.log(area)
