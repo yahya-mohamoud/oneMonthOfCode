c3dedd9734728a8fa912b633b564b2f1490b8fa7
yahya-mohamoud <yahyamohamoud625@gmail.com.com>
2025-04-20 22:30:11 +0300

    day three exercise baby

---
+++ dayTwo/script.js
@@ -0,0 +1,8 @@
+function getAttributes() {
+    const para = document.querySelector('#w3r');
+    const test = para.getAttribute('href')
+    // console.log(test)
+    return test;
+  }
+  
+  getAttributes()
